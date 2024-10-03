4ebdffb84bf13fc8ac4dc964efbb25f45efa2c93
Silence20240313 <liangxiaoli378@gmail.com>
2024-10-04 08:30:30 +0900

    10.4

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -2,7 +2,9 @@ package com.jiawa.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.jiawa")
 @SpringBootApplication
 public class WikiApplication {
 
--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//  TODO Controller层的注解
 @RestController
 public class TestController {
-    // 接口 get put 都可以
-    @RequestMapping("/hello")
+    @Value("${test.hello}")
+    private String testHello;
+    // TODO 要让方法变成一个接口 加注解@RequestMapping  支持所有的请求方式
+    //  常见的http请求方式：get post put delete  查询/新增/修改/删除
+    @GetMapping ("/hello")
     public String hello(){
-        return  "Hello World";
+        return "Hello World" + testHello;
     }
 }
