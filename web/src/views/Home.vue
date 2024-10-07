3e13702aa51ce88cc60cd08e5757074a8acbfc10
Silence20240313 <liangxiaoli378@gmail.com>
2024-10-07 11:06:31 +0900

    add frontend ^ backend integration

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -51,16 +51,34 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      Content
+      <pre>
+        {{ ebooks }}
+      </pre>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent,onMounted,ref } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+
+  setup(){
+    console.log("setup");
+    const ebooks = ref();
+    onMounted( () =>{
+      console.log("onMounted");
+      axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
+        const data = response.data;
+        console.log(data.content);
+        ebooks.value = data.content;
+      });
+    })
+    return{
+       ebooks
+    }
+  }
 });
 </script>
