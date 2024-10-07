269d0502c04f49b0c20b8fd4953f38563e9be339
Silence20240313 <liangxiaoli378@gmail.com>
2024-10-07 22:17:55 +0900

    展示单条数据

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -2,8 +2,6 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
       >
@@ -51,17 +49,61 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-        {{ ebooks }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="ebooks">
+
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
+            </a-list-item-meta>
+
+          </a-list-item>
+        </template>
+      </a-list>
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
 // @ts-nocheck
-import { defineComponent,onMounted,ref } from 'vue';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import { defineComponent,onMounted,ref,reactive,toRef } from 'vue';
 import axios from 'axios';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+const actions: Record<string, any>[] = [
+  { icon: StarOutlined, text: '156' },
+  { icon: LikeOutlined, text: '156' },
+  { icon: MessageOutlined, text: '2' },
+];
 
 export default defineComponent({
   name: 'Home',
@@ -69,17 +111,24 @@ export default defineComponent({
   setup(){
     console.log("setup");
     const ebooks = ref();
+    const ebooks1 = reactive({books:[]});
 
     onMounted( () =>{
       console.log("onMounted");
       axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
+        console.log(response)
         const data = response.data;
         console.log(data.content);
         ebooks.value = data.content;
+        ebooks1.books = data.content;
       });
-    })
+    });
     return{
-       ebooks
+      ebooks,
+      ebooks2:toRef(ebooks1,"books"),
+      listData,
+      pagination,
+      actions
     }
   }
 });
