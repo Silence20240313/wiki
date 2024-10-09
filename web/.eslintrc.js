c73bf29e2c95c4223e652aed3a6b22189b9b58ad
Silence20240313 <liangxiaoli378@gmail.com>
2024-10-10 07:44:35 +0900

    10.10

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -16,5 +16,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-unused-components': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',  // 禁用不允许使用 @ts-ignore/@ts-nocheck 注释的规则
+    '@typescript-eslint/no-explicit-any': 0,
+    'vue/no-unused-vars': 0,
   }
 }
